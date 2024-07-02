3a2a92a0ed765d3bb13ff9bc222f226f8773f205
Micah Effiong <micaiah.effiong@gmail.com>
2024-07-02 18:21:14 +0100

    chore(setup) secondary window

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,37 +1,82 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import electron, { app, shell, BrowserWindow, ipcMain } from "electron";
+import { join } from "path";
+import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import icon from "../../resources/icon.png?asset";
 
-function createWindow(): void {
+function createMainWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
+    // width: 900,
+    // height: 670,
+    // show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    ...(process.platform === "linux" ? { icon } : {}),
     webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
-  })
+      preload: join(__dirname, "../preload/index.js"),
+      sandbox: false,
+    },
+  });
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+  mainWindow.on("ready-to-show", () => {
+    // mainWindow.show();
+    mainWindow.maximize();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+    shell.openExternal(details.url);
+    return { action: "deny" };
+  });
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
+  }
+}
+
+console.log(process.env["ELECTRON_RENDERER_URL"], { "is.dev": is.dev });
+
+function createSecondaryWindow(): void {
+  const screens = electron.screen.getAllDisplays();
+  // Create the browser window.
+  const secondaryWindow = new BrowserWindow({
+    // width: 900,
+    // height: 670,
+    // show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === "linux" ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, "../preload/index.js"),
+      sandbox: false,
+    },
+  });
+
+  secondaryWindow.on("ready-to-show", () => {
+    // mainWindow.show();
+    secondaryWindow.maximize();
+    if (screens.length > 1) {
+      const secScreen = screens[0];
+      secondaryWindow.setSize(secScreen.size.width, secScreen.size.height);
+      secondaryWindow.setPosition(secScreen.bounds.x, secScreen.bounds.y);
+      secondaryWindow.setFullScreen(true);
+    }
+  });
+
+  secondaryWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url);
+    return { action: "deny" };
+  });
+
+  // HMR for renderer base on electron-vite cli.
+  // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    secondaryWindow.loadURL(
+      process.env["ELECTRON_RENDERER_URL"] + "/secondary.html",
+    );
+  } else {
+    secondaryWindow.loadFile(join(__dirname, "../renderer/secondary.html"));
   }
 }
 
@@ -40,35 +85,40 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-
+  electronApp.setAppUserModelId("com.electron");
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+  app.on("browser-window-created", (_, window) => {
+    optimizer.watchWindowShortcuts(window);
+  });
+
+  console.log(electron.screen.getAllDisplays());
 
   // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
+  ipcMain.on("ping", () => console.log("pong"));
 
-  createWindow()
+  createMainWindow();
+  createSecondaryWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+      createSecondaryWindow();
+    }
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
---
+++ src/renderer/src/secondary/main.tsx
@@ -0,0 +1,11 @@
+import "@renderer/primary/assets/main.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <div>testing</div>
+  </React.StrictMode>,
+);
