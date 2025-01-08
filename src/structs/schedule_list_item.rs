dd4d352e0692ab8e9457738ca5c7d69a97c71f1a
Micah Effiong <micaiah.effiong@gmail.com>
2025-01-08 18:35:28 +0100

    add backgound to schedule
    Backgrounds from preview can be added to schedule when a song item is
    sent to schedule.
    The Search widget tracks active preview backgrounds
    
    Schedule list items now track thier backgrounds instead of the
    schedule-viewer the way sending schedule data to preview include the
    image directly and individual items can have seperate backgounds

--- src/structs/schedule_list_item.rs
+++ src/structs/schedule_list_item.rs
@@ -6,16 +6,35 @@ use relm4::{gtk, typed_view::list::RelmListItem, view};
 pub struct ScheduleListItemModel {
     pub list: Vec<String>,
     pub title: String,
+    pub backgound_image: Option<String>,
 }
 
 impl ScheduleListItemModel {
-    pub fn new(title: String, list: Vec<String>) -> Self {
-        return ScheduleListItemModel { title, list };
+    pub fn new(title: String, list: Vec<String>, bg: Option<String>) -> Self {
+        return ScheduleListItemModel {
+            title,
+            list,
+            backgound_image: bg,
+        };
+    }
+
+    fn format_bg_style(image: &str) -> String {
+        let mut style = format!(
+            "background-size: cover; background-position: center center; background-color: black;",
+        );
+
+        if !image.is_empty() {
+            let bg_image_style = format!("background-image: url(\"file://{}\");", image);
+            style = style + &bg_image_style;
+        }
+
+        return style;
     }
 }
 
 pub struct ScheduleListItemWidget {
     pub label: gtk::Label,
+    pub preview_box: gtk::Box,
     // pub note: Option<gtk::Label>,
 }
 
@@ -34,6 +53,7 @@ impl RelmListItem for ScheduleListItemModel {
             list_item_box = gtk::Box {
                 set_margin_all: 6,
 
+                #[name="preview_box"]
                 gtk::Box {
                     set_height_request: 30,
                     set_width_request: 70,
@@ -62,12 +82,16 @@ impl RelmListItem for ScheduleListItemModel {
             }
         }
 
-        let widgets = ScheduleListItemWidget { label };
+        let widgets = ScheduleListItemWidget { label, preview_box };
 
-        (list_item_box, widgets)
+        return (list_item_box, widgets);
     }
 
     fn bind(&mut self, widgets: &mut Self::Widgets, _root: &mut Self::Root) {
         widgets.label.set_label(&self.title);
+
+        if let Some(bg) = &self.backgound_image {
+            widgets.preview_box.inline_css(&Self::format_bg_style(&bg));
+        }
     }
 }
