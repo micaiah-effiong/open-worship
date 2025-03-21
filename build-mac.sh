ac6781706677873888b6311d16a5f9f1909629fd
Micah Effiong <micaiah.effiong@gmail.com>
2025-03-21 15:28:22 +0100

    setup mac build

---
+++ build-mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BUILD_DIR="build/macos"
+CURRENT_DIR=$(pwd)
+
+# build bundle
+# ensure cargo-bundle is install
+cargo bundle --release --format osx
+
+# clean build dir
+rm -r $BUILD_DIR
+mkdir -p $BUILD_DIR
+cp -r target/release/bundle/osx/Openworship.app $BUILD_DIR
+cd build/macos
+
+# link dylib
+dylibbundler -od -b -x Openworship.app/Contents/MacOS/open-worship -d Openworship.app/Contents/Resources/libs -p @executable_path/../Resources/libs
+
+# reset dir
+cd $CURRENT_DIR
